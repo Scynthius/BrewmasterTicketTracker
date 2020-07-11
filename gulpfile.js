0be1ee6440236ade4d2996ad0630afaba583df13
Steve Peters <steve.james.peters@gmail.com>
2020-07-11 01:23:31 -0500

    Started adding Create New functionality
    Started the groundwork for the Create New functionality. Implemented modal to be used for Create New.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 const del = require('del');
 
-gulp.task('compile', function () {
+gulp.task('sass', function () {
     return gulp.src('./scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./public/styles/'));
+});
+
+gulp.task('uglify', function() {
+    return gulp.src('./scripts/*.js')
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest('./public/scripts/'));
 });
 
 gulp.task('clean', function () {
@@ -15,4 +22,4 @@ gulp.task('clean', function () {
     ]);
 });
 
-gulp.task('default', gulp.series(['clean','compile']));
+gulp.task('default', gulp.series(['clean','sass', 'uglify']));
