28b6cdc306edf8cffbde6669b631a3e04fc461ad
Steve Peters <steve.james.peters@gmail.com>
2020-07-25 20:27:44 -0500

    SQL Queries and UI Overhaul

--- SQLQueries.sql
+++ SQLQueries.sql
@@ -32,77 +32,23 @@ JOIN Categories ON Tickets.CategoryID = Categories.CategoryID
 JOIN Clients ON Tickets.ClientID = Clients.ClientID
 WHERE Tickets.Status = "Closed"
 
---Select Specified Ticket for ticket_details page
-SELECT Tickets.Title, Tickets.Description, Clients.ClientName as Client, Categories.Name AS Category, Tickets.Status, 
-DATE_FORMAT(Tickets.SubmitDate, "%m/%d/%Y") AS Submitted, DATE_FORMAT(Tickets.ModifiedDate, "%m/%d/%Y") AS Modified,
-DATE_FORMAT(Tickets.CloseDate, "%m/%d/%Y") AS Closed, Tickets.Resolution  FROM Tickets
-JOIN Categories ON Tickets.CategoryID = Categories.CategoryID
-JOIN Clients ON Tickets.ClientID = Clients.ClientID
-WHERE Tickets.TicketID = req.body.id
+--Insert New Category
+INSERT INTO Categories (`Name`, `CreatedDate`) VALUES (?, ?)
 
---Select assigned employees for ticket_details page
-SELECT CONCAT(FirstName, ' ', LastName) AS Name
-FROM Assignments JOIN Employees ON Assignments.EmployeeID = Employees.EmployeeID
-WHERE Assignments.TicketID = req.body.id
+--Insert New Client
+INSERT INTO Clients (`ClientName`, `PrimaryContact`, `Email`, `Phone`) VALUES (?,?,?,?)
 
---Select clients, categories, employees for ticket_details page
-SELECT Clients.ClientID, Clients.ClientName as Client FROM Clients
-SELECT Categories.CategoryID, Categories.Name as Category FROM Categories
-SELECT Employees.EmployeeID, CONCAT(FirstName, ' ', LastName) as name FROM Employees
+--Insert New Ticket
+INSERT INTO Tickets (`Description`, `ClientID`, `CategoryID`, `Status`, `SubmitDate`) VALUES (?,?,?,?,?)
 
---Update Tickets table
-UPDATE Tickets SET Title=req.body.title, Description=req.body.description, ClientID=(SELECT ClientID FROM Clients WHERE ClientName=req.body.client), 
-CategoryID=(SELECT CategoryID FROM Categories WHERE Name=req.body.category),
-ModifiedDate=req.body.modifiedDate,  
-Status=req.body.status, SubmitDate=req.body.submitdate, 
-CloseDate=(CASE WHEN (req.body.status <=> "Closed") then (req.body.closeDate) ELSE(null) End), Resolution=req.body.resolution
-WHERE TicketID=req.body.ticketid
- 
---Update Assignment table
-DELETE FROM Assignments WHERE TicketID=req.body.ticketid;
-INSERT INTO Assignments (EmployeeID, TicketID) VALUES ((SELECT EmployeeID FROM Employees WHERE CONCAT(FirstName, ' ', LastName)=req.body.employee), req.body.ticketid);
+--Insert New Employee
+INSERT INTO Employees (`FirstName`, `LastName`, `Email`, `AccessLevel`) VALUES (?,?,?,?)
 
---Delete Tickets table entry. Requires Assignments to be removed first.
-DELETE FROM Assignments WHERE TicketID=req.body.ticketid;
-DELETE FROM Tickets WHERE TicketID=req.body.ticketid;
+--Insert New BusinessType
+INSERT INTO BusinessTypes (`Name`, `CreatedDate`) VALUES (?,?)
 
---Select Specified Client for client_details page
-SELECT Clients.ClientName, Clients.ClientName, Clients.PrimaryContact, Clients.Email, Clients.Phone  FROM Clients
-WHERE Clients.ClientID = req.body.id
+--Assign Ticket/Insert New Assignment
+INSERT INTO Assignments (`EmployeeID`, `TicketID`) VALUES (?,?)
 
---Select assigned business types for client_details page
-SELECT BusinessTypes.Name AS Types FROM ClientTypes 
-JOIN BusinessTypes ON BusinessTypes.TypeID = ClientTypes.TypeID
-WHERE ClientTypes.ClientID = req.body.id
-
--- Select all business types for client_details page
-SELECT BusinessTypes.Name as Types FROM BusinessTypes
-
---Update Client entries
-UPDATE Clients SET ClientName=req.body.clientname, PrimaryContact=req.body.primarycontact, Email=req.body.email, Phone=req.body.phone
-WHERE ClientID = req.body.clientid
-
---Update ClientTypes entry
-DELETE FROM ClientTypes WHERE ClientID=req.body.clientid;
-INSERT INTO ClientTypes (ClientID, TypeID) VALUES (req.body.clientid, (SELECT TypeID FROM BusinessTypes WHERE Name=req.body.clienttype));
-
---Delete Clients table entry. Requires ClientType to be removed first.
-DELETE FROM ClientTypes WHERE ClientID=req.body.clientid;
-DELETE FROM Clients WHERE CliendID=req.body.clientid;
-
---Select specified Employee for employee_details page
-SELECT FirstName, LastName, Email, AccessLevel FROM Employees WHERE EmployeeID = req.body.id
-
---Select specified business type for business_details page
-SELECT Name FROM BusinessTypes WHERE TypeID = req.body.id
-
---Select specified category for category_details page
-SELECT Name FROM Categories WHERE CategoryID = req.body.id
-
---Update Employee
-UPDATE Employees SET FirstName=req.body.firstname, LastName=req.body.lastname, Email=req.body.email, AccessLevel=req.body.accesslevel
-WHERE EmployeeID=req.body.employeeid
-
---Delete Employees table entry. Requires Assignments to be removed first.
-DELETE FROM Assignments WHERE EmployeeID=req.body.employeeid;
-DELETE FROM Employees WHERE EmployeeID=req.body.employeeid;
+--Assign Business to Clients/Insert New ClientType
+INSERT INTO ClientTypes (`ClientID`, `TypeID`) VALUES (?,?)
--- index.js
+++ index.js
@@ -9,9 +9,6 @@ const { resolve } = require('path');
 
 const SELECT_CLIENTS = 'SELECT Clients.ClientId, Clients.ClientName as Client FROM Clients';
 const SELECT_CATEGORIES = 'SELECT Categories.CategoryId, Categories.Name as Category FROM Categories';
-const SELECT_UNASSIGNED = 'SELECT Tickets.Title, Tickets.Description, Tickets.Status, Tickets.ClientID, Clients.ClientName, Categories.Name as Category, DATE_FORMAT(Tickets.SubmitDate, "%m/%d/%Y") AS Submitted FROM Tickets JOIN Clients as Clients ON Tickets.ClientID = Clients.ClientID JOIN Categories as Categories ON Tickets.CategoryID = Categories.CategoryID WHERE Tickets.Status = "Unassigned"';
-const SELECT_ASSIGNED = 'SELECT Tickets.TicketID, Tickets.Title, Tickets.Description, Categories.Name as Category, Tickets.Status, Clients.ClientId, Clients.ClientName, GROUP_CONCAT(CONCAT(Employees.EmployeeID, ":", Employees.FirstName, " ", Employees.LastName) SEPARATOR ",") AS AssignedEmployees, DATE_FORMAT(Tickets.SubmitDate, "%m/%d/%Y") AS Submitted, DATE_FORMAT(Tickets.ModifiedDate, "%m/%d/%Y") AS LastUpdated FROM Tickets JOIN Assignments ON Tickets.TicketID = Assignments.TicketID JOIN Employees ON Assignments.EmployeeID = Employees.EmployeeID JOIN Categories ON Tickets.CategoryID = Categories.CategoryID JOIN Clients ON Tickets.ClientID = Clients.ClientID WHERE Tickets.Status = "Assigned" GROUP BY Tickets.TicketID';
-const SELECT_CLOSED = 'SELECT Tickets.Title, Tickets.Description, Categories.Name as Category, Clients.ClientID, Clients.ClientName, DATE_FORMAT(Tickets.CloseDate, "%m/%d/%Y") AS Closed, Tickets.Resolution FROM Tickets JOIN Categories ON Tickets.CategoryID = Categories.CategoryID JOIN Clients ON Tickets.ClientID = Clients.ClientID WHERE Tickets.Status = "Closed"'
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -26,45 +23,23 @@ app.set('port', process.argv[2]);
 
 app.get('/',function(req,res){
   var context = {};
+  var sqlPool = mysql.pool;
 
-  getQuery(SELECT_CLIENTS)
+  getQuery(sqlPool, SELECT_CLIENTS)
   .then((rows) => { 
     context.clients = rows;
-    return getQuery(SELECT_CATEGORIES);
+    return getQuery(sqlPool, SELECT_CATEGORIES);
   })
   .then((rows) => {
     context.categories = rows;
-    return getQuery(SELECT_UNASSIGNED);
-  })
-  .then((rows) => {
-    context.unassigned = rows;
-    return getQuery(SELECT_ASSIGNED);
-  })
-  .then((rows) => {
-    context.assigned = rows;
-    for (var i = 0; i < context.assigned.length; i++) {
-      var assigned = context.assigned[i].AssignedEmployees;
-      var result = assigned.split(/,|:/);
-      var employees = [];
-      for (var j = 0; j < result.length; j += 2) {
-        employees.push({
-          "id" : result[j],
-          "name" : result[j+1]
-        });
-      }
-      context.assigned[i].AssignedEmployees = employees;
-    }
-    return getQuery(SELECT_CLOSED);
-  })
-  .then((rows) => {
-    context.closed = rows;
+    console.log(context);
     res.render('dashboard', context);
   });
 });
 
-function getQuery(query) {
+function getQuery(pool, query) {
   return new Promise((resolve, reject) => {
-    mysql.pool.query(query, function (err, rows, fields) {
+    pool.query(query, function (err, rows, fields) {
       if (err) {
         return reject(err);
       }
@@ -101,33 +76,6 @@ app.post('/', function (req, res, next) {
     case "New Ticket":
       sqlPool.query('INSERT INTO Tickets (`Description`, `ClientID`, `CategoryID`, `Status`, `SubmitDate`) VALUES (?,?,?,?,?)',
       [req.body.Description, req.body.ClientID, req.body.CategoryID, req.body.Status, req.body.SubmitDate], function (err, result) {
-        if (err) {
-          next(err);
-          return;
-        }
-        res.sendStatus(200);
-      });
-      break;
-  }
-});
-
-app.post('/', function (req, res, next) {
-  var requestType = req.body.requestType;
-  var sqlPool = mysql.pool;
-  switch (requestType) {
-    case "New Category":
-      sqlPool.query('INSERT INTO Categories (`Name`, `CreatedDate`) VALUES (?, ?)',
-      [req.body.name, req.body.date], function (err, result) {
-        if (err) {
-          next(err);
-          return;
-        }
-        res.sendStatus(200);
-      });
-      break;
-    case "New Client":
-      sqlPool.query('INSERT INTO Clients (`ClientName`, `PrimaryContact`, `Email`, `Phone`) VALUES (?,?,?,?)',
-      [req.body.ClientName, req.body.PrimaryContact, req.body.Email, req.body.Phone], function (err, result) {
         if (err) {
           next(err);
           return;
